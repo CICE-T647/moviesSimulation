4504fe5cc0e8ac1733aca29975fcce8b1691206d
tritio <tritio2@gmail.com>
2019-11-27 20:58:47 +0100

    resuelto ejercicio de callback e inicio de promise

--- methods.js
+++ methods.js
@@ -1,8 +1,43 @@
-// getMovieById
+const peliculas = require("./moviesData");
+
+const getMovieById = (id, callback) => {
+    const pelicula = peliculas.find((peli) => {
+        return peli.id == id;
+    })
+    if(pelicula) {
+        callback(null, pelicula)
+    }
+    else {
+        callback("no se ha encontrado esa película")
+    }
+}
 
 
+getMovieById(2, (error, data) => {
+    setTimeout(() => {
+        if(error) {
+            console.log(error);
+        } 
+        return console.log(data)
+    }, 2000)
+})
 
-// getMovieByTitle
+const getMovieByTitle = (title) => {
+    return new Promise((resolve, reject) => { 
+        setTimeout(() => {
+            const pelis = peliculas.map((peli) => {
+                peli.title.startsWith(title)
+            })    
+            resolve(pelis)
+        }, 2000)       
+            
+    })
+}
+
+getMovieByTitle("The")
+.then((resultado) => {   
+       console.log(resultado.length)   
+})
 
 
 
