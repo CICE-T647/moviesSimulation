db8341b068b5308678d5d5175c3de6febbfaee08
olatzaranzabe <olatzaranzabe@gmail.com>
2019-11-27 21:58:52 +0100

    do callback

--- methods.js
+++ methods.js
@@ -1,9 +1,22 @@
+const movies = require("./moviesData");
+
 // getMovieById
 
+const getMovieById = (id, callback) => {
+  setTimeout(() => {
+    const movie = movies.movies.find(movie => {
+      return movie.id === id;
+    });
+    if (!movie) callback(console.log("no hay peli con este id"));
+    else callback(null, movie);
+  }, 2000);
+};
 
+getMovieById(3, (error, data) => {
+  if (error) return console.log(error);
+  return console.log(data);
+});
 
 // getMovieByTitle
-
-
 
 // getMovieByShowtimes
