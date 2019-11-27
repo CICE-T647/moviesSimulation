1356979cfa671e44a2123205b5303b0062fba331
Asier Cidon <acidonpe@redhat.com>
2019-11-27 21:59:16 +0100

    Added getMovieById function and a call example

--- methods.js
+++ methods.js
@@ -1,9 +1,27 @@
+const movies = require("./moviesData");
+
+// console.log(movies);
+
 // getMovieById
 
+const getMovieById = (id, callback) => {
+  console.log("Initializating Movie search by ID", id);
+  setTimeout(() => {
+    const movie = movies.movies.find(movie => {
+      return movie.id === id;
+    });
+    if (!movie) callback(`Not exist a movie with this ID ${id}`);
+    else callback(null, movie);
+  }, 3000);
+};
 
+getMovieById(8, (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data);
+});
 
 // getMovieByTitle
-
-
 
 // getMovieByShowtimes
