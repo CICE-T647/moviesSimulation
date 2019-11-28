90434c0b657d89d1c82f7b6749be3af01b310fca
unknown <villarjimenez00@gmail.com>
2019-11-28 19:05:01 +0100

    creacion e inicializacion de modal e input timerpicker

--- methods.js
+++ methods.js
@@ -1,9 +1,23 @@
+const movies = require("./moviesData.js");
 // getMovieById
+const getMovieById = (id, cb) => {
+  const movie = movies.find(movie => {
+    return movie.id == id;
+  });
 
-
-
+  if (!movie) {
+    cb("No existe la pelicula");
+  } else {
+    cb(null, movie);
+  }
+};
+getMovieById(5, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
 // getMovieByTitle
-
-
 
 // getMovieByShowtimes
