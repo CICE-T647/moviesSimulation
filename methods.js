65fea4a4b29470535c3c94d33bc1dca7be74eba8
Gonzalo G <coalescencia1@gmail.com>
2019-11-28 09:24:19 +0100

    finalizado ejercicios moviesSimulation

--- methods.js
+++ methods.js
@@ -1,5 +1,7 @@
 const peliculas = require("./moviesData");
 
+    
+
 const getMovieById = (id, callback) => {
     const pelicula = peliculas.find((peli) => {
         return peli.id == id;
@@ -11,24 +13,38 @@ const getMovieById = (id, callback) => {
         callback("no se ha encontrado esa película")
     }
 }
-
+                
 
 getMovieById(2, (error, data) => {
     setTimeout(() => {
+        console.log("********************************************************");
+        console.log("                   CON CALLBACK                         ");
+        console.log("********************************************************");
         if(error) {
             console.log(error);
         } 
         return console.log(data)
-    }, 2000)
-})
+    }, 1000)
+}) 
+
+
 
 const getMovieByTitle = (title) => {
     return new Promise((resolve, reject) => { 
         setTimeout(() => {
-            const pelis = peliculas.map((peli) => {
-                peli.title.startsWith(title)
-            })    
-            resolve(pelis)
+            console.log("********************************************************");
+            console.log("                   CON PROMESAS                         ");
+            console.log("********************************************************"); 
+            const pelis = peliculas.filter((peli) => {              
+              return peli.title.startsWith(title);
+            })
+            if(pelis.length > 0){               
+                resolve(pelis)
+            }
+             reject(`No se ha encontrado ninguna película que empiece por ${title}`)
+                
+                    
+            
         }, 2000)       
             
     })
@@ -36,9 +52,37 @@ const getMovieByTitle = (title) => {
 
 getMovieByTitle("The")
 .then((resultado) => {   
-       console.log(resultado.length)   
+       console.log(resultado)   
 })
+.catch(error => console.log(error)) 
 
+   
 
+ const getMovieByShowtimes = (times)=> {
+    return new Promise((resolve, reject) => { 
+        setTimeout(() => {
+            console.log("********************************************************");
+            console.log("                   CON ASYNC - AWAIT                    ");
+            console.log("********************************************************");   
+            const horarios = peliculas.filter(peli => {
+                return peli.showtimes.includes(times);
+            })
+            if(horarios.length > 0) {
+                resolve(horarios)
+            }
+            reject(`No se ha encontrado ninguna película que comience a las ${times}`)
+        }, 4000)
+    })
+ }
 
-// getMovieByShowtimes
+ const getShowTimes = async () => {
+     try {
+         const result = await getMovieByShowtimes("15:30");  // espera a que se ejecute getMovieByShowTimes() para pasarle el valor a result
+         console.log(result);
+     }
+     catch(error) {
+        console.log(error)
+     }
+}
+
+getShowTimes(); 
