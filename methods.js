7b3dcb13b491055ff51e69245200d8da5a40df0f
Jose Angel <joseangel@kreativework.com>
2019-11-29 12:51:24 +0100

    Practica finalizada

--- methods.js
+++ methods.js
@@ -1,9 +1,86 @@
+const movies = require('./moviesData.js');
+
 // getMovieById
+
+/* const getMovieById = (id, callback) => {
+
+    const movie = movies.find(movie => {
+        return movie.id === id;
+    })
+
+    if (!movie) callback(`No hay ninguna pelicula con el id ${id}`);
+    else callback(null, movie);
+
+}
+
+getMovieById(50, (error, data) => {
+    if (error) return console.log(error);
+    return console.log(data);
+}); */
+
+
 
 
 
 // getMovieByTitle
 
+/* const getMovieFromMoviesByTitle = (e) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const pelis = [];
+            movies.forEach(movie => {
+                if (movie.title.startsWith(e)) {
+                    pelis.push(movie.title);
+                }
+            }); 
+
+            if(pelis.length==0){
+                reject("No hay pelis");
+            }
+
+            resolve(pelis);
+
+            
+            
+        }, 3000)
+    });
+}
+
+const getMovieByTitle = (e) => {
+    getMovieFromMoviesByTitle(e)
+    .then(pelis => console.log(pelis))
+    .catch(error => console.log(error));
+
+};
+
+getMovieByTitle("x"); */
+
+
 
 
 // getMovieByShowtimes
+
+const getMovieFromMoviesByShowTime = (e) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const moviesMatched = movies.filter(movie => movie.showtimes.includes(e))
+
+            if(moviesMatched.length==0){
+                reject("No se ha encontrado niguna peli con este horario");
+            }
+            resolve(moviesMatched);   
+        }, 3000)
+    });
+}
+
+const getMoviesByShowTime = async (e) => {
+    try {
+        const moviesmatched = await getMovieFromMoviesByShowTime(e);
+        return console.log(moviesmatched);
+    } catch (error) {
+        return console.log(error);
+    }
+   
+};
+
+getMoviesByShowTime("13:70");
