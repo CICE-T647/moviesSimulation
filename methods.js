754257565106a89b4f86494ad5f2a342c45bd63e
Asier Cidon <acidonpe@redhat.com>
2019-12-01 20:01:47 +0100

    Merged master

--- methods.js
+++ methods.js
@@ -22,3 +22,45 @@ const getMovieById = (id, callback) => {
         else callback(null, movie);
     }, 3000);
 };
+
+// getMovieByTitle
+
+const getMovieByTitle = async title => {
+    console.log("Initializating Movie search by Title", title);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let matches = [];
+            for (let i = 0; i < movies.movies.length; i++) {
+                if (movies.movies[i].title.startsWith(title)) {
+                    matches.push(movies.movies[i]);
+                }
+            }
+            if (matches.length > 0) resolve(matches);
+            else reject(`Opss - Movies with Pattern "${title}" not found`);
+        }, 3000);
+    });
+};
+
+// getMovieByShowtimes
+const getMovieByShowtimes = async time => {
+    console.log("Initializating Movie search by Time", time);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let matches = [];
+            for (let i = 0; i < movies.movies.length; i++) {
+                if (movies.movies[i].showtimes.includes(time)) {
+                    matches.push(movies.movies[i]);
+                }
+            }
+            if (matches.length > 0) resolve(matches);
+            else reject(`Opss - Movies showed on ${time} not found`);
+        }, 3000);
+    });
+};
+
+module.exports = {
+    getMovieById,
+    getMovies,
+    getMovieByTitle,
+    getMovieByShowtimes
+};
