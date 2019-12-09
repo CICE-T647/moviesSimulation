a33d54fc50c63eefedaa3349666cb89173af4ca9
Carlos Velez <cfvelez9@gmail.com>
2019-12-09 18:31:29 +0100

    #CICE - Servidor Node - Cfvelez - modificado un solo endpoint y por parametros http://localhost:3000/query?showtime=13:00

--- index.js
+++ index.js
@@ -1,93 +1,94 @@
 const http = require("http");
 const url = require("url");
-const querystring = require("querystring");
-
-const PORT = 3000;
+const qstring = require("querystring");
 
 const { 
-    getMovieFromMovieDataById, 
+    getMovieFromMoviesDataById, 
     getMoviesFromMoviesDataByTitle, 
-    getMoviesFromMoviesDataByShowtime 
+    getMoviesFromMoviesDataByShowtimes 
 } = require("./methods")
 
 
 const server = http.createServer(async (request, response) => {
 
-    const { query } = url.parse(request.url);
+    const parsedUrl = url.parse(request.url);
 
-    const { id, title, showtime } = querystring.parse(query);
+    const query = parsedUrl.query;
+
+    const { id, title, showtime } = qstring.parse(query);
 
     switch ( request.url ) {
 
-        case `/getmoviebyid?${ query }`:
+        case `/query?${ query }`:
 
             response.setHeader("Content-Type", "text/plain")
 
-            if (query && id) {
+            if (query) {
+                
+                if(id){
+                    
+                    if(!isNaN(id)){
 
-                getMovieFromMovieDataById(parseInt(id), (error, data) => {
-                    if (error) {
-                        response.statusCode = error.status ? error.status : 500
-                        response.end(error.message ? error.message : "Internal server error.");
+                        getMovieFromMoviesDataById(parseInt(id), (error, data) => {
+                            if (error) {
+                                response.statusCode = error.status ? error.status : 500
+                                response.end(error.message ? error.message : "Error de servidor.");
+                            }
+                            response.statusCode = 200;
+                            response.end(JSON.stringify(data))
+                        });
                     }
-                    response.statusCode = 200;
-                    response.end(JSON.stringify(data))
-                });
+                    else{
+                        response.statusCode = 422
+                        response.end("Debe ingresar un número para el id");
+                    }
+                }
+                else if(title){
+
+                    getMoviesFromMoviesDataByTitle(title)
+                    .then((movies)=> {
+                    response.statusCode = 200
+                    response.end(JSON.stringify(movies))
+                    })
+                    .catch((error) => {
+                        response.statusCode = error.status ? error.status : 500
+                        response.end(error.message ? error.message : "Internal server error")
+                    });
+                }
+                else if(showtime){
+                    try {
+
+                        const data = await getMoviesFromMoviesDataByShowtimes(showtime)
+                        response.statusCode = 200
+                        response.end(JSON.stringify(data))
+        
+                    } catch (error){
+        
+                        console.log(error)
+                        response.statusCode = error.status ? error.status : 500
+                        response.end(error.message ? error.message : "Internal server error")
+        
+                    }
+
+                }
+                else{
+                    response.statusCode = 422
+                    response.end("Debe proporcionar un parametro de busqueda correcto. (id, time or showtime)ej: query?id");
+                }
+                
 
             } else {
                 response.statusCode = 422
-                response.end("Debe proporcionar una id")
+                response.end("Debe proporcionar un parametro de busqueda. (id, time or showtime) ej: query?id=");
             }
         break;
 
-        case `/getmoviesbytitle?${ query }`:
-            response.setHeader("Content-Type", "text/plain");
-
-            if( !query || !title ) {
-                response.statusCode = 422
-                response.end("Debe proporcionar un título")
-            }
-
-            getMoviesFromMoviesDataByTitle(title)
-            .then((movies)=> {
-                response.statusCode = 200
-                response.end(JSON.stringify(movies))
-            })
-            .catch((error) => {
-                response.statusCode = error.status ? error.status : 500
-                response.end(error.message ? error.message : "Internal server error")
-            })
-            break;
-
-        case `/getmoviesbyshowtime?${ query }`:
-
-            response.setHeader("Content-Type", "text/plain");
-
-            if ( !query || !showtime ) {
-                response.statusCode = 422
-                response.end("Debe proporcionar una hora")
-            }
-
-            try {
-
-                const data = await getMoviesFromMoviesDataByShowtime(showtime)
-                response.statusCode = 200
-                response.end(JSON.stringify(data))
-
-            } catch (error){
-
-                console.log(error)
-                response.statusCode = error.status ? error.status : 500
-                response.end(error.message ? error.message : "Internal server error")
-
-            }
-                break;
         default: 
             response.statusCode = 404
-            response.end("The route your are asking for doesn't exist")
+            response.end("La ruta no existe");
             
     }
 
 });
 
-server.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+server.listen(3000, () => console.log(`Servidor corriendo en el puerto 3000`));
